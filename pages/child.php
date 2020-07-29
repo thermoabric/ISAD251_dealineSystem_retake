d87fd5a988b6f3eeb8c0fb041bdc3fc7407c9f66
Jake <34009334+thermoabric@users.noreply.github.com>
2020-07-29 10:13:02 +0100

    procedures from sql server are being created to reduce reuse of code on website side

--- pages/child.php
+++ pages/child.php
@@ -4,8 +4,14 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-
-<?php
-        include './pages\\Home.php'
-         ?>
-  
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
