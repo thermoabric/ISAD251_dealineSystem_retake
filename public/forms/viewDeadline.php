547914d6a911f99052ff9330e6faaccf33d8c27b
Jake <34009334+thermoabric@users.noreply.github.com>
2020-08-03 16:16:50 +0100

    improved code readability on viewDeadline

--- public/forms/viewDeadline.php
+++ public/forms/viewDeadline.php
@@ -7,66 +7,67 @@ include_once "serverConnection.php";
 //$result = mysqli_query($conn,"CALL getAllDeadlinesAndAppointments ($childNameInHtml)");
 
 $result = mysqli_query($conn," SELECT * FROM deadlinesandappointments WHERE childName = '$childNameInHtml' ");
-
-
-
 ?>
-<!DOCTYPE html>
+
+
+
 <?php
 if(isset($_POST["submitChild"])) { 
 ?>
+
+    <html>
+        <?php
+        if(isset($_POST['childNameIn']) && !empty($_POST['childNameIn'])){
+          ?>
+
+            <body>
+                <?php
+                if (mysqli_num_rows($result) > 0) {
+                    ?>
+
+
+                    <table>
+
+                        <tr>
+                          <td>deadlineId</td>
+                          <td>deadlineName</td>
+                          <td>datetimeset</td>
+                          <td>childName</td>
+                          <td>details</td>
+                          <td>notes</td>
+                        </tr>
+                        <?php
+                        $i=0;
+                        while($row = mysqli_fetch_array($result)) {
+                        ?>
+                            <tr>
+                                <td><?php echo $row["deadlineId"]; ?></td>
+                                <td><?php echo $row["deadlineName"]; ?></td>
+                                <td><?php echo $row["childName"]; ?></td>
+                                <td><?php echo $row["dateTimeset"]; ?></td>
+                                <td><?php echo $row["details"]; ?></td>
+                                <td><?php echo $row["notes"]; ?></td>
+                            </tr>
+                        <?php
+                        $i++;
+                        }
+                        ?>
+                    </table>
+                <?php
+                }
+                else{
+                    echo "No result found";
+                }
+                ?>
+     
+            </body>
+        <?php
+        }
+        ?>    
+    </html>
     
-<html>
-    <?php
-  if(isset($_POST['childNameIn']) && !empty($_POST['childNameIn'])){
-      ?>
- 
-<body>
-<?php
-if (mysqli_num_rows($result) > 0) {
-?>
-  
-  
-  <table>
-  
-  <tr>
-    <td>deadlineId</td>
-    <td>deadlineName</td>
-    <td>datetimeset</td>
-    <td>childName</td>
-    <td>details</td>
-    <td>notes</td>
-    
-    
-  </tr>
-<?php
-$i=0;
-while($row = mysqli_fetch_array($result)) {
-?>
-<tr>
-    <td><?php echo $row["deadlineId"]; ?></td>
-    <td><?php echo $row["deadlineName"]; ?></td>
-    <td><?php echo $row["childName"]; ?></td>
-    <td><?php echo $row["dateTimeset"]; ?></td>
-    <td><?php echo $row["details"]; ?></td>
-    <td><?php echo $row["notes"]; ?></td>
-</tr>
-<?php
-$i++;
+<?php   
 }
-?>
-</table>
- <?php
-}
-else{
-    echo "No result found";
-}
-?>
- </body>
-</html>
-<?php
-}
-?>
-<?php
-}
+?> 
+
 ?>
